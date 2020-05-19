d277acaa323d0b5d5c32137d87515c10786f2d51
Julia Wakeman <wakeman.j@rangiruru.school.nz>
2020-05-19 17:25:43 +1200

    v2
    Expected: The code lets you choose how many charcters you want, and you can choose whether you want special charcters, numbers and or letters. They are in checkbox form so you can choose what ever you want of them.
    
    boundry: the code does nothing after you press the creat password button

--- src/index.svelte
+++ src/index.svelte
@@ -1,3 +1,36 @@
+<script>
+    let userLength = ''
+    let password = ''
+    
+    function makePassword() {
+         
+    }
+</script>
+<style>
+    label {
+        padding: 10px;
+    }
+</style>
+
 <h1>Password Maker</h1>
 
 <p>Create a secure password!</p>
+
+<label>
+How many charcters do you want your password?<input type="number" bind:value={userLength}>
+</label>
+<h2>Choose your contents:</h2>
+<label>
+Letters:<input type="checkbox">
+</label>
+<lable>
+Numbers:<input type="checkbox">
+</lable>
+<label>
+Special Charcters:<input type="checkbox">
+</label>
+
+<button on:click={makePassword}>Make Password!</button>
+
+<p>Here is your password:</p>
+<p>{password}</p>
