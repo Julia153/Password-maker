9a099fd57fff6a74ba3a9b97165ced74c9d3b958
Julia Wakeman <wakeman.j@rangiruru.school.nz>
2020-06-02 09:45:54 +1200

    v5 Made code stronger
    Expected: when you choose 1 check box at a time it will output a password with your chosen charcter at the length of your choosing. tested with 12 and lowercase and 5, tested with 11 and 7 for uppercase, tested with 6 and 9 for numbers, tested with 4 and 12 for special charcters
    
    Boundry: if you choose mutiple checkboxes it only uses the first one you clicked not a mix of both

--- src/index.svelte
+++ src/index.svelte
@@ -1,74 +1,114 @@
 <script>
-    const userMaxLength = 20
-    const userMinLength = 4
+  const userMaxLength = 20;
+  const userMinLength = 4;
 
-    let userLength = ''
-    let userSC = false
-    let userLowerLetters = false
-    let userUpperletters = false
-    let userNumbers = false
-    let output = ''
-    let password = ''
-    
-    function makePassword() {
-        let counter = 0
-         if (userLength > userMaxLength) {
-             output = "Please enter a number of charcters that is less than 20"
-         } else if (userLength < userMinLength) {
-             output = "Please enter a number of charcters that is more than 0"
-         } else if (!userLowerLetters && !userNumbers && !userSC && !userUpperletters) {
-             output = "Click at least one checkbox"
-         } else if (userLowerLetters && !userUpperletters && !userNumbers && !userSC) {
-             while (counter < userLength) {  
-                let choiceInNumbers = Math.floor(Math.random() * 26) + 97
-                output += String.fromCharCode(choiceInNumbers)
-                counter ++
-             } if (counter > userLength) {
-                 console.log(password)
-                output = password
-                password = ''
-                counter = 0
-             }
-             console.log(output)
-         }
-    
+  let userLength = "";
+  let userSC = false;
+  let userLowerLetters = false;
+  let userUpperletters = false;
+  let userNumbers = false;
+  let output = "";
+  let password = "";
+
+  function makePassword() {
+    output = "";
+    if (userLength > userMaxLength) {
+      output = "Please enter a number of charcters that is less than 20";
+    } else if (userLength < userMinLength) {
+      output = "Please enter a number of charcters that is more than 0";
+    } else if (
+      !userLowerLetters &&
+      !userNumbers &&
+      !userSC &&
+      !userUpperletters
+    ) {
+      output = "Click at least one checkbox";
     }
+    lengthPasswordCreater();
+  }
 
-  
+  function lengthPasswordCreater() {
+    for (let i = 0; i < userLength; i++) {
+      if (userLowerLetters) {
+        let choiceInNumbersForLowerLetters =
+          Math.floor(Math.random() * 26) + 97;
+        output += String.fromCharCode(choiceInNumbersForLowerLetters);
+        console.log(output);
+      } else if (userUpperletters) {
+        let choiceInNumbersForUpperLetters =
+          Math.floor(Math.random() * 26) + 65;
+        output += String.fromCharCode(choiceInNumbersForUpperLetters);
+      } else if (userNumbers) {
+        let choiceInNumbersForNumbers = Math.floor(Math.random() * 10) + 48;
+        output += String.fromCharCode(choiceInNumbersForNumbers);
+      } else if (userSC) {
+        let choiceInNumbersForSC = Math.floor(Math.random() * 4) + 35;
+        output += String.fromCharCode(choiceInNumbersForSC);
+      }
+    }
+  }
 </script>
-<style>
-    label {
-        padding: 10px;
-    }
 
-    button {
-        display: block;
-    }
+<style>
+  label {
+    padding: 10px;
+  }
+
+  button {
+    display: block;
+  }
 </style>
+
 <!--HTML-->
 <section class="section content">
-<h1>Password Maker</h1>
+  <h1>Password Maker</h1>
 
-<p>Create a secure password!</p>
+  <p>Create a secure password!</p>
 
-<h2>Choose your contents:</h2>
-<label>
-How many charcters do you want your password?<input class="input" type="number" min="1" max="20" bind:value={userLength}>
-</label>
-<label>
-Lowercase Letters:<input id="lowerLettersCheckbox" class="checkbox"type="checkbox" bind:checked={userLowerLetters}>
-</label>
-<label>
-Uppercase Letters:<input id="UpperLettersCheckbox" class="checkbox" type="checkbox" bind:checked={userUpperletters}>
-</label>
-<lable>
-Numbers:<input id="numbersCheckbox" class="checkbox" type="checkbox" bind:checked={userNumbers}>
-</lable>
-<label>
-Special Charcters:<input id="SpecialCharctersCheckbox" class="checkbox"type="checkbox" bind:checked={userSC}>
-</label>
-<br>
-<button class="button" on:click={makePassword}>Make Password!</button>
-<p>Here is your password:</p>
-<p>{output}</p>
+  <h2>Choose your contents:</h2>
+  <label>
+    How many charcters do you want your password?
+    <input
+      class="input"
+      type="number"
+      min="1"
+      max="20"
+      bind:value={userLength} />
+  </label>
+  <label>
+    Lowercase Letters:
+    <input
+      id="lowerLettersCheckbox"
+      class="checkbox"
+      type="checkbox"
+      bind:checked={userLowerLetters} />
+  </label>
+  <label>
+    Uppercase Letters:
+    <input
+      id="UpperLettersCheckbox"
+      class="checkbox"
+      type="checkbox"
+      bind:checked={userUpperletters} />
+  </label>
+  <lable>
+    Numbers:
+    <input
+      id="numbersCheckbox"
+      class="checkbox"
+      type="checkbox"
+      bind:checked={userNumbers} />
+  </lable>
+  <label>
+    Special Charcters:
+    <input
+      id="SpecialCharctersCheckbox"
+      class="checkbox"
+      type="checkbox"
+      bind:checked={userSC} />
+  </label>
+  <br />
+  <button class="button" on:click={makePassword}>Make Password!</button>
+  <p>Here is your password:</p>
+  <p>{output}</p>
 </section>
